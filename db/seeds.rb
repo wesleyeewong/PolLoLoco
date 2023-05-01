d25fc246e82b1b84d7ca482182c8322517d2ea80
Wesley Wong <wesleywong1993@gmail.com>
2023-05-01 18:33:49 +0200

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Migration.say_with_time("Seeding movements ...") do
+  movement_slugs = YAML.load_file(Rails.root.join("db", "seeds", "movements.yml"))
+  @movements = Movement.create!(movement_slugs.map { |slug| { slug: } })
+end
+
+ActiveRecord::Migration.say_with_time("Seeding users and profiles ...") do
+  wesley = User.create!(email: "wesley@polloloco.com", name: "Wesley")
+  wesley.create_profile!(name: "Wesley")
+
+  ActiveRecord::Migration.say_with_time("Seeding progressions ...") do
+    wesley.profile.progressions.create!(
+      initial_reps: 5, initial_sets: 5, initial_weight: 0,
+      max_reps: 10, max_sets: 5, min_reps: 5, min_sets: 5,
+      name: "Some progression", rep_increments: 1, weight_increments: 2.5,
+      movement: @movements.first
+    )
+  end
+end
