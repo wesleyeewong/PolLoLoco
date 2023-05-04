afcc192707c8c7960da83fb8a9fcb9ff1765252c
Wesley Wong <wesleywong1993@gmail.com>
2023-05-04 14:18:13 +0200

    create completed set migration

---
+++ app/models/completed_set.rb
@@ -0,0 +1,3 @@
+class CompletedSet < ApplicationRecord
+  belongs_to :progression_assignment
+end
---
+++ spec/factories/completed_sets.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :completed_set do
+    reps { 1 }
+    weight { "9.99" }
+    progression_assignment { nil }
+  end
+end
